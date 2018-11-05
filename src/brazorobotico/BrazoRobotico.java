d38b848382d57c49490f506dccc173c4dc24f9bb
Cinthia Nava <Cinthia Nava@DESKTOP-E818I7A>
2018-11-04 18:51:03 -0600

    V1 apliacion brazo robotico

---
+++ src/brazorobotico/BrazoRobotico.java
@@ -0,0 +1,23 @@
+
+package brazorobotico;
+
+/**
+ *Instituto Tecnologico de Leon
+ *Ingenieria en Sistemas Computacionales 
+ * @authores: 
+ * Gonzalez Alcaraz Hernan Arturo
+ * Nava Torres Juana Cinthia Lizbeth
+ * Padilla Guerrero Paul Adrian
+ * "Brazo Robotico"
+ * Martes 13 de Noviembre del 2018
+ * Semestre Agosto - Diciembre 
+ */
+public class BrazoRobotico {
+
+    
+    public static void main(String[] args) {
+        AppBrazoRobotico brazo = new AppBrazoRobotico();
+        brazo.lanzar();
+    }
+    
+}
